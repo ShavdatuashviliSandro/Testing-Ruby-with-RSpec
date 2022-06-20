760c3b543d9c2bf5f8dbcd1d5c372dc930380719
ShavdatuashviliSandro <sandro.shav@gmail.com>
2022-06-20 17:20:13 +0400

    uploaded project

---
+++ spec/card_spec.rb
@@ -0,0 +1,55 @@
+class Card
+  attr_reader :type, :rank, :suit
+  def initialize(type,rank,suit)
+    @type = type
+    @rank = rank
+    @suit = suit
+  end
+  def get_type
+    @type
+  end
+end
+RSpec.describe 'Card' do
+  card = Card.new('Card type','Card rank', 'Card suit')
+  it 'has a type' do
+    expect(card.type).to eq('Card type')
+  end
+
+  it 'has a rank' do
+    expect(card.rank).to eq('Card rank')
+  end
+
+  it 'has a suit' do
+    expect(card.suit).to eq('Card suit')
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ANSWER
+#
+# RSpec.describe 'math calculations' do
+#   it 'does basic math' do
+#     expect(1+2).to eq(3)
+#     expect(10-2).to eq(8)
+#     expect(3*4).to eq(12)
+#     expect(10/2).to eq(5)
+#   end
+# end
